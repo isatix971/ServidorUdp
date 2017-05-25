556357dc8cfc26f472620f45792eb13c0da7fa07
fvergarh <fvergarh@SAN-H9PC162.usersad.everis.int>
2017-05-25 00:12:06 -0300

    asdf

---
+++ src/serverudp/ServerUdp.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverudp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author fvergarh
+ */
+public class ServerUdp {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static Connection cnx = null;
+
+    public static Connection obtener() throws SQLException, ClassNotFoundException {
+        if (cnx == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                cnx = DriverManager.getConnection("jdbc:mysql://localhost/gpssace", "root", "lalola1415");
+            } catch (SQLException ex) {
+                throw new SQLException(ex);
+            } catch (ClassNotFoundException ex) {
+                throw new ClassCastException(ex.getMessage());
+            }
+        }
+        return cnx;
+    }
+
+    public static void cerrar() throws SQLException {
+        if (cnx != null) {
+            cnx.close();
+        }
+    }
+
+    public static void main(String[] args) {
+System.out.println("ñldkjñlkfd");
+    }
+
+}

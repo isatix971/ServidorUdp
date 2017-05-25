283cebfa3b6b9159663302522eda978c17688ab4
fvergarh <fvergarh@SAN-H9PC162.usersad.everis.int>
2017-05-25 00:15:54 -0300

    asdf

--- src/serverudp/ServerUdp.java
+++ src/serverudp/ServerUdp.java
@@ -5,9 +5,14 @@
  */
 package serverudp;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -40,8 +45,36 @@ public class ServerUdp {
         }
     }
 
-    public static void main(String[] args) {
-System.out.println("ñldkjñlkfd");
+    public static void main(String[] args) throws Exception{
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData());
+            String[] fragmento = sentence.split(",");
+
+//            for (int i = 0; i < fragmento.length; i++) {
+//                System.out.println(i + ": " + fragmento[i]);
+//            }
+            Connection conex = obtener();
+
+            Statement stmt = conex.createStatement();
+
+            String sql;
+            sql = "INSERT INTO coordenadas (id, x, y, comentario) VALUES (NULL, '3', '4', 'dfsg');";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            System.out.println("RECEIVED: " + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket
+                    = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
     }
 
 }
